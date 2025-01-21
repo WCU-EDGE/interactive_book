ab98a6f8b2e4906752fc219bff9130979dc12998
Linh B Ngo <lngo@wcupa.edu>
2025-01-21 08:44:57 -0500

    Delete build.bat

--- build.bat
+++
@@ -1,7 +0,0 @@
-echo on
-
-docker image rm book-master:latest 
-docker image rm book-worker:latest 
-
-docker build -t book-master:latest ./docker/master
-docker build -t book-worker:latest ./docker/worker
