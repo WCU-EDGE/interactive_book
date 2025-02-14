e0103bc61d32b6cc8e8351a675edcec5c1051887
Linh B Ngo <lngo@wcupa.edu>
2025-02-13 23:04:26 -0500

    Update install.sh

--- docker/bookcase/install.sh
+++ docker/bookcase/install.sh
@@ -3,6 +3,10 @@
 # setup code server
 sudo sh /home/student/build/code-server.sh --prefix=/usr/local/
 
+# modify to install more extensions here ...
+sudo code-server --install-extension ms-python.python
+sudo code-server --install-extension ms-toolsai.jupyter
+
 # setup Python packages
 source /opt/env/python3/bin/activate
 sudo pip install jupyter jupyter-book mkdocs-material markdown-include mkdocs-table-reader-plugin mkdocs-glightbox mkdocs-jupyter  
