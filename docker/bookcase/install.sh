cf6ed5149a7485be7cdac0eafda14c1a921a7e66
Linh B Ngo <lngo@wcupa.edu>
2025-02-14 08:52:26 -0500

    Update install.sh

--- docker/bookcase/install.sh
+++ docker/bookcase/install.sh
@@ -7,9 +7,15 @@ sudo sh /home/student/build/code-server.sh --prefix=/usr/local/
 sudo code-server --install-extension ms-python.python
 sudo code-server --install-extension ms-toolsai.jupyter
 
+# setup Nodejs 20.0.0 for jupyter-book 2.0
+curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+source /home/student/.bashrc
+nvm install 20.0.0
+nvm use 20.0.0
+
 # setup Python packages
 source /opt/env/python3/bin/activate
-sudo pip install jupyter "jupyter-book==2.0.0a1" markdown-include nodejs
+sudo pip install jupyter "jupyter-book==2.0.0a1" markdown-include
 
 sudo cp /home/student/build/entrypoint.sh /usr/local/bin/entrypoint.sh
 sudo chmod 755 /usr/local/bin/entrypoint.sh
